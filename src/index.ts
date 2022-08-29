905942942bfa7b99ddb3c80e4b265b2eaf4f636f
Andrea Martinez <andrea.maort@gmail.com>
2022-08-29 19:23:57 +0200

    added coverage sonar exclusions for starServer and loadEnvironment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import connectDB from "./database";
 import "./loadEnvironment";
-import startServer from "./server/server";
+import startServer from "./server/startServer";
 
 const port = +process.env.PORT || 4000;
 const mongoURL = process.env.MONGODB_URL;
